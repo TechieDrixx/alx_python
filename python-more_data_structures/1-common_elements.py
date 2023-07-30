eab3bc0023f97a44bc4d601b85c64ca5c90288e5
TechieDrixx <kaydee12000@gmail.com>
2023-07-30 08:10:51 -0700

    script

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for list_ in matrix:
-        if not list_:
-            print()
-        for item in list_:
-            if item == list_[-1]:
-                print('{:d}'.format(item))
-            else:
-                print('{:d}'.format(item), end=" ")
+def common_elements(set_1, set_2):
+    return set(set_1) & set(set_2)
