17c301940e8f246f33b0bb74529e690b3e743ad6
TechieDrixx <kaydee12000@gmail.com>
2023-07-30 08:07:07 -0700

    script

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,23 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    my_string = my_string.translate({ord("c"): None})
-    my_string = my_string.translate({ord("C"): None})
-    return my_string
+def square_matrix_simple(matrix=[]):
+    # Create a new matrix to store the squared values
+    result_matrix = []
+    
+    # Iterate over each row in the input matrix
+    for row in matrix:
+        # Create a new row for the result matrix
+        result_row = []
+        
+        # Iterate over each element in the current row and compute its square
+        for element in row:
+            squared_value = element ** 2
+            result_row.append(squared_value)
+        
+        # Add the current row to the result matrix
+        result_matrix.append(result_row)
+    
+    return result_matrix
+
+# Test the function
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+result = square_matrix_simple(matrix)
+print(result)
