5fcdfb2c232bf0e4ceeaa7541add8c19fba23975
TechieDrixx <kaydee12000@gmail.com>
2023-07-30 04:02:19 -0700

    script

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,10 +1,10 @@
-import sys
+    import sys
 
 def print_arguments(args):
     num_arguments = len(args) - 1
 
     print("Number of argument(s):", num_arguments, end=' ')
-    print("argument" if num_arguments == 1 else "arguments", end=' ')
+    print("argument" if num_arguments == 1 else "arguments", end='')
     print(":" if num_arguments > 0 else ".", end='\n')
 
     for i in range(1, num_arguments + 1):
