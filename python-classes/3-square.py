2710d65130fbca9dfe373f598549cfa70dd45fc8
TechieDrixx <kaydee12000@gmail.com>
2023-08-06 03:56:04 -0700

    script

---
+++ python-classes/3-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0):
+            Initializes a square object with a given size (default size is 0).
+        area(self):
+            Returns the area of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a square object with a given size.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Args:
+            value (int): The size to be set.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
