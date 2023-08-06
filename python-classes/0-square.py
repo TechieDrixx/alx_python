ef8ab11125a656986227114e31f79268dcfa2f64
TechieDrixx <kaydee12000@gmail.com>
2023-08-06 03:37:26 -0700

    script

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,14 +2,7 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-    def get_size(self):
-        return self.__size
-
-    def set_size(self, new_size):
-        if new_size > 0:
-            self.__size = new_size
-        else:
-            raise ValueError("Size must be a positive value.")
-
-    def area(self):
-        return self.__size ** 2
+# Example usage
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
