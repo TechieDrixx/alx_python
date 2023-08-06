7e4e548193446ff0cc3aa441e41e8aa9e57906d1
TechieDrixx <kaydee12000@gmail.com>
2023-08-06 04:31:17 -0700

    script

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,17 @@
+"""
+This module checks if an object is the instance of a class
+
+"""
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
