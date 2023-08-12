4a7403cd7d87450b2d3f01184fa94de07704bc33
TechieDrixx <kaydee12000@gmail.com>
2023-08-11 22:31:12 -0700

    script

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,22 @@
-# import requests module
+#!/usr/bin/env python3
+"""
+Module to fetch and display the status of a website.
+"""
+
 import requests
- 
-try:
-    response = requests.get('https://alu-intranet.hbtn.io/status')
- 
-    if response.status_code == 200:
-        print(response.text)
-    else:
-        print(f"Request failed with status code:{response.status_code}")
-    response = requests.get('https://alu-intranet.hbtn.io/status')
- 
-    if response.status_code == 200:
-        print(response.text)
-    else:
-        print(f"Request failed with status code: {response.status_code}")
- 
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
+
+def fetch_hbtn_status():
+    """
+    Fetches and displays the status of https://alu-intranet.hbtn.io/status.
+    """
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+    content_type = type(response.text)
+    content = response.text
+   
+    print("Body response:")
+    print(f"\t- type: {content_type}")
+    print(f"\t- content: {content}")
+   
+if __name__ == "__main__":
+    fetch_hbtn_status()
