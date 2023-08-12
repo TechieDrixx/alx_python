dda84a53776e21e310f204905ee6d8406e7dc8d9
TechieDrixx <kaydee12000@gmail.com>
2023-08-11 22:46:54 -0700

    script

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,24 +1,23 @@
-import requests
+#!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
 import sys
+import requests
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <letter>")
-        return
-
-    letter = sys.argv[1]
-    payload = {'q': letter}
-
-    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
-
-    try:
-        json_data = response.json()
-        if json_data:
-            print("[{}] {}".format(json_data['id'], json_data['name']))
-        else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
 
 if __name__ == "__main__":
-    main()
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
