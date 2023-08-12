414b33ba93757c603a2aa2cacb8f375ff021e882
TechieDrixx <kaydee12000@gmail.com>
2023-08-11 22:42:25 -0700

    script

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,18 +1,40 @@
-#!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
+File Edit Options Buffers Tools Python Help                                                                                                                  
+#!/usr/bin/python3                                                                                                                                           
+"""                                                                                                                                                          
+This is a Python script that takes in a URL and an email address,                                                                                            
+sends a POST request to the passed URL with the email as a parameter,                                                                                        
+and finally displays the body of the response.                                                                                                               
 """
+import requests
 import sys
-import urllib.parse
-import urllib.request
 
+def send_post_request(url, email):
+    """                                                                                                                                                      
+    Sends a POST request to the provided URL with the email as a parameter and displays the response body.                                                   
+                                                                                                                                                             
+    Args:                                                                                                                                                    
+        url (str): The URL to send the POST request to.                                                                                                      
+        email (str): The email address to include as a parameter in the request.                                                                             
+                                                                                                                                                             
+    Raises:                                                                                                                                                  
+        requests.exceptions.RequestException: If an error occurs during the HTTP request.                                                                    
+    """
+    payload = {"email": email}
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    response = requests.post(url, data=payload)
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    response.raise_for_status()
+
+    print("Response body:")
+    print(response.text)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Please provide a URL and an email address as command-line arguments.")
+    else:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        try:
+            send_post_request(url, email)
+        except requests.exceptions.RequestException as e:
+            print("Error:", str(e))
