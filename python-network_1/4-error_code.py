8941cd8c24794b158c1f3938552c168bce928a07
TechieDrixx <kaydee12000@gmail.com>
2023-08-11 22:44:49 -0700

    script

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,29 +1,18 @@
-import requests
+#!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
 import sys
+import requests
 
-def get_github_user_id(username, pat):
-    try:
-        url = "https://api.github.com/user"
-        response = requests.get(url, auth=(username, pat))
 
-        if response.status_code == 200:
-            user_info = response.json()
-            user_id = user_info.get('id')
-            if user_id:
-                print("GitHub User ID:", user_id)
-            else:
-                print("User ID not found in the response")
-        else:
-            print("Error:", response.text)
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-if _name_ == "_main_":
-    if len(sys.argv) != 3:
-        print("Usage: ./github_user_id.py <Nyapar> <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>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    pat = sys.argv[2]
-
-    get_github_user_id(Nyapar, pat)
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
+        
